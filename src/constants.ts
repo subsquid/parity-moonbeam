2834690cd00026eb4631476e1072500b826d11e7
Rikonorus <djavad199450@gmail.com>
2022-03-15 17:01:20 +0300

    init

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const CHAIN_NAME = "moonbeam";
+export const STATUS_DB_NAME = "chain_status";
+export const BATCH_SIZE = 500;
+export const ARCHIVE_ENDPOINT =
+  "https://moonbeam-beta.indexer.gc.subsquid.io/v4/graphql";
+export const CHAIN_ENDPOINT = "wss://moonbeam.api.onfinality.io/public-ws";
